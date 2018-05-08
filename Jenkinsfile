80da33d69d1bd5e574574a26593bcb5953f856de
Mauricio Giraldo <mauro2357@gmail.com>
2018-05-08 16:50:06 -0500

    no message

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,6 +1,7 @@
 // This shows a simple build wrapper example, using the AnsiColor plugin.
 	pipeline { 
 		agent any 
+		triggers { pollSCM('* * * * *') }
 		stages {
 			stage('Build') { 
 				steps { 
---
+++ KitBasicoAutomApp/src/test/java/unit/co/com/valtica/kitbasico/AfiliadoTest.java
@@ -0,0 +1,12 @@
+package unit.co.com.valtica.kitbasico;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class AfiliadoTest {
+	
+	public void assertThan() {
+		//mock(IEmpleadoDAO.class);
+	}
+
+}
