2dd8bcfbac5cc2538a5ba5cfc7e65b4669dc5174
Mauricio Giraldo <mauro2357@gmail.com>
2018-05-14 03:04:38 -0500

    no message

--- anali_code.bat
+++ anali_code.bat
@@ -1,2 +1,2 @@
 cd KitBasicoAutomApp
-gradlew sonarqube -Dsonar.organization=mauro2357-github -Dsonar.host.url=https://localhost:9000 -Dsonar.login=75364d3fe0e15eec82e9dff49b93cceed78bfbd5
+gradlew sonarqube -Dsonar.host.url=http://localhost:9000 -Dsonar.login=75364d3fe0e15eec82e9dff49b93cceed78bfbd5
